8cdc6d3138687817e4d48bbe6d6c583865e285be
tracer-ics2016 <tracer@njuics.org>
2017-04-13 02:29:43 -0400

    >  run 151221111 root Linux kali 4.3.0-kali1-amd64 #1 SMP Debian 4.3.5-1kali1 (2016-02-11) x86_64 GNU/Linux  02:29:43 up  4:34,  7 users,  load average: 0.04, 0.04, 0.05 402d5def8a1a9a3d1914ea81c783a14d94f897

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -153,7 +153,7 @@ static void info_reg() {
 
 static int cmd_x(char *args) {
     uint32_t n, addr, i, j;
-    sscanf(args, "%u %x", &n, &addr);
+    sscanf(args, "%u %u", &n, &addr);
 
     for(i = 0; i < n; i++){
         printf("0x%08x:\t", addr);
