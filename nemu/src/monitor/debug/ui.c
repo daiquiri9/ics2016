d43d79fb035869847a309f8745d955018f39a228
tracer-ics2016 <tracer@njuics.org>
2017-04-13 02:57:44 -0400

    >  run 151221111 root Linux kali 4.3.0-kali1-amd64 #1 SMP Debian 4.3.5-1kali1 (2016-02-11) x86_64 GNU/Linux  02:57:44 up  5:02,  7 users,  load average: 0.00, 0.01, 0.05 1e832281167faf8c8362e83668a2f09efea0a044

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -155,10 +155,10 @@ static int cmd_x(char *args) {
     uint32_t n, addr, i, j;
     sscanf(args, "%u %x", &n, &addr);
 
-    for(i = 0; i < n; i++){
+    for(i = 0; i < n; i += 4){
         printf("0x%08x:\t", addr);
         for(j = 0; (i + j) < n && j < 4; j++){
-            uint32_t info = swaddr_read(addr + ((i + j) * 4), 4);
+            uint32_t info = swaddr_read(addr + (i + j) * 4, 4);
             printf("0x%08x\t", info);
         }
         printf("\n");
